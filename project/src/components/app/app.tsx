5b617b937f9cd06a9fdf849262719104e5e095e5
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-12 19:19:33 +0400

    Add Layout to Routes

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,5 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Layout from '../layout/layout';
 import MainScreen from '../../pages/main-screen/main-screen';
 import LoginScreen from '../../pages/login-screen/login-screen';
 import RoomScreen from '../../pages/room-screen/room-screen';
@@ -9,11 +10,13 @@ function App({placeCount}: {placeCount: number}): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<MainScreen placeCount={placeCount} />} />
-        <Route path='/login' element={<LoginScreen />} />
-        <Route path='/favorites' element={<FavoritesScreen />} />
-        <Route path='/offer/:id' element={<RoomScreen />} />
-        <Route path='*' element={<NotFoundScreen />} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<MainScreen placeCount={placeCount} />} />
+          <Route path='/login' element={<LoginScreen />} />
+          <Route path='/favorites' element={<FavoritesScreen />} />
+          <Route path='/offer/:id' element={<RoomScreen />} />
+          <Route path='*' element={<NotFoundScreen />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
