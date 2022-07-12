4c331bf83c38569582daac175feecdfcbcad56aa
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-12 19:38:30 +0400

    Remove unnecessary Icons component

--- project/src/components/layout/layout.tsx
+++ project/src/components/layout/layout.tsx
@@ -1,13 +1,7 @@
 import { Outlet } from 'react-router-dom';
-import Icons from '../icons/icons';
 
 function Layout(): JSX.Element {
-  return (
-    <>
-      <Icons />
-      <Outlet />
-    </>
-  );
+  return <Outlet />;
 }
 
 export default Layout;
