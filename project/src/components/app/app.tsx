2da664d2642ab71522fba32383450d85159937bb
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-13 04:00:27 +0400

    Create PrivateRoute component

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,25 +1,33 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from '../layout/layout';
 import MainScreen from '../../pages/main-screen/main-screen';
 import LoginScreen from '../../pages/login-screen/login-screen';
 import RoomScreen from '../../pages/room-screen/room-screen';
 import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
 import Page404Screen from '../../pages/page-404-screen/page-404-screen';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   placeCount: number;
-}
+};
 
-function App({placeCount}: AppProps): JSX.Element {
+function App({ placeCount }: AppProps): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<MainScreen placeCount={placeCount} />} />
-          <Route path='/login' element={<LoginScreen />} />
-          <Route path='/favorites' element={<FavoritesScreen />} />
-          <Route path='/offer' element={<RoomScreen />} />
-          <Route path='*' element={<Page404Screen />} />
+          <Route path="/login" element={<LoginScreen />} />
+          <Route
+            path="/favorites"
+            element={
+              <PrivateRoute>
+                <FavoritesScreen />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/offer" element={<RoomScreen />} />
+          <Route path="*" element={<Page404Screen />} />
         </Route>
       </Routes>
     </BrowserRouter>
