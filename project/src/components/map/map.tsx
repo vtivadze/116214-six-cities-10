32661ebc7d793f1096b8600d61d12cf742530aa7
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-22 00:36:17 +0400

    Add markers to map

--- project/src/components/map/map.tsx
+++ project/src/components/map/map.tsx
@@ -1,9 +1,12 @@
-import { useRef } from 'react';
-import leafleat from 'leaflet';
+import { useRef, useEffect } from 'react';
+import leaflet from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 import { Offer } from '../../types/offer';
 import { CITY } from '../../constants';
+import useMap from '../../hooks/useMap';
+
+import {URL_MARKER_DEFAULT, URL_MARKER_CURRENT} from '../../constants';
 
 type MapProps = {
   offers: Offer[];
@@ -11,6 +14,34 @@ type MapProps = {
 
 function Map({offers}: MapProps): JSX.Element {
   const mapRef = useRef(null);
+  const map = useMap(mapRef, CITY);
+
+  const defaultCustomIcon = leaflet.icon({
+    iconUrl: URL_MARKER_DEFAULT,
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
+
+  const currentCustomIcon = leaflet.icon({
+    iconUrl: URL_MARKER_CURRENT,
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
+
+  useEffect(() => {
+    if (map) {
+      offers.forEach((offer) => {
+        leaflet
+          .marker({
+            lat: offer.city.location.latitude,
+            lng: offer.city.location.longitude,
+          }, {
+            icon: defaultCustomIcon,
+          })
+          .addTo(map);
+      });
+    }
+  }, [map, offers, defaultCustomIcon]);
 
   return (
     <section className="cities__map map" ref={mapRef}></section>
--- project/src/constants.ts
+++ project/src/constants.ts
@@ -1,5 +1,7 @@
 export const PLACE_COUNT = 312;
 export const STARS_COUNT = 5;
+export const URL_MARKER_DEFAULT = 'img/pin.svg';
+export const URL_MARKER_CURRENT = 'img/pin-active.svg';
 
 export enum AppRoute {
   Root = '/',
