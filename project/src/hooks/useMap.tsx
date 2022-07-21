4e9b08b6b046250c096ad80deed25be76f7278e7
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-21 23:22:38 +0400

    Display map

---
+++ project/src/hooks/useMap.tsx
@@ -0,0 +1,43 @@
+import {MutableRefObject, useEffect, useState, useRef} from 'react';
+import leaflet from 'leaflet';
+import {Map} from 'leaflet';
+
+type City = {
+  title: string;
+  lat: number;
+  lng: number;
+  zoom: number;
+}
+
+function useMap(mapRef: MutableRefObject<HTMLElement | null>, city: City): Map | null {
+  const [map, setMap] = useState<Map | null>(null);
+  const isRenderedRef = useRef<boolean>(false);
+
+  useEffect(() => {
+    if (mapRef.current !== null && !isRenderedRef.current) {
+      const instance = leaflet.map(mapRef.current, {
+        center: {
+          lat: city.lat,
+          lng: city.lng,
+        },
+        zoom: city.zoom,
+      });
+
+      leaflet
+        .tileLayer(
+          'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+          {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+          },
+        )
+        .addTo(instance);
+
+      setMap(instance);
+      isRenderedRef.current = true;
+    }
+  }, [mapRef, city]);
+
+  return map;
+}
+
+export default useMap;
