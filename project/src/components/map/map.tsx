bf9ecd032e19ae4f6a30350de8f36064218b9762
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-22 00:40:05 +0400

    --amend

--- project/src/components/map/map.tsx
+++ project/src/components/map/map.tsx
@@ -6,13 +6,13 @@ import { Offer } from '../../types/offer';
 import { CITY } from '../../constants';
 import useMap from '../../hooks/useMap';
 
-import {URL_MARKER_DEFAULT, URL_MARKER_CURRENT} from '../../constants';
+import { URL_MARKER_DEFAULT } from '../../constants';
 
 type MapProps = {
   offers: Offer[];
-}
+};
 
-function Map({offers}: MapProps): JSX.Element {
+function Map({ offers }: MapProps): JSX.Element {
   const mapRef = useRef(null);
   const map = useMap(mapRef, CITY);
 
@@ -22,30 +22,31 @@ function Map({offers}: MapProps): JSX.Element {
     iconAnchor: [20, 40],
   });
 
-  const currentCustomIcon = leaflet.icon({
-    iconUrl: URL_MARKER_CURRENT,
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
-  });
+  // const currentCustomIcon = leaflet.icon({
+  //   iconUrl: URL_MARKER_CURRENT,
+  //   iconSize: [40, 40],
+  //   iconAnchor: [20, 40],
+  // });
 
   useEffect(() => {
     if (map) {
       offers.forEach((offer) => {
         leaflet
-          .marker({
-            lat: offer.city.location.latitude,
-            lng: offer.city.location.longitude,
-          }, {
-            icon: defaultCustomIcon,
-          })
+          .marker(
+            {
+              lat: offer.city.location.latitude,
+              lng: offer.city.location.longitude,
+            },
+            {
+              icon: defaultCustomIcon,
+            }
+          )
           .addTo(map);
       });
     }
   }, [map, offers, defaultCustomIcon]);
 
-  return (
-    <section className="cities__map map" ref={mapRef}></section>
-  );
+  return <section className="cities__map map" ref={mapRef}></section>;
 }
 
 export default Map;
