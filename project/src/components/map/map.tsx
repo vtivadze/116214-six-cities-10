ba7418a95183b891d0db8bad4a969dbf84a93240
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-23 16:57:37 +0400

    Get rid of unwnated commented code snippets

--- project/src/components/map/map.tsx
+++ project/src/components/map/map.tsx
@@ -22,12 +22,6 @@ function Map({ offers }: MapProps): JSX.Element {
     iconAnchor: [20, 40],
   });
 
-  // const currentCustomIcon = leaflet.icon({
-  //   iconUrl: URL_MARKER_CURRENT,
-  //   iconSize: [40, 40],
-  //   iconAnchor: [20, 40],
-  // });
-
   useEffect(() => {
     if (map) {
       offers.forEach((offer) => {
