b0cd2f3c2b402d420682813a5e5a4d8bf9e5a576
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-25 10:21:42 +0400

    Create ReviewItem component

--- project/src/pages/offer-screen/offer-screen.tsx
+++ project/src/pages/offer-screen/offer-screen.tsx
@@ -8,6 +8,7 @@ import { reviews } from '../../mocks/reviews';
 import Header from '../../components/header/header';
 import CommentForm from '../../components/comment-form/comment-form';
 import OfferItem from '../../components/offer-item/offer-item';
+import ReviewItem from '../../components/review-item/review-item';
 
 import { calculateRatingPercentage } from '../../utils';
 
@@ -122,54 +123,24 @@ function OfferScreen(): JSX.Element {
                   <p className="property__text">{offer.description}</p>
                 </div>
               </div>
-              {reviews.length > 0 && (
-                <section className="property__reviews reviews">
-                  <h2 className="reviews__title">
-                    Reviews &middot;{' '}
-                    <span className="reviews__amount">{reviews.length}</span>
-                  </h2>
+              <section className="property__reviews reviews">
+                <h2 className="reviews__title">
+                  Reviews &middot;{' '}
+                  <span className="reviews__amount">{reviews.length}</span>
+                </h2>
+                {reviews.length > 0 && (
                   <ul className="reviews__list">
                     {reviewsToDisplay.map((review) => (
-                      <li key={review.id} className="reviews__item">
-                        <div className="reviews__user user">
-                          <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                            <img
-                              className="reviews__avatar user__avatar"
-                              src={review.avatar}
-                              width="54"
-                              height="54"
-                              alt="Reviews avatar"
-                            />
-                          </div>
-                          <span className="reviews__user-name">
-                            {review.author}
-                          </span>
-                        </div>
-                        <div className="reviews__info">
-                          <div className="reviews__rating rating">
-                            <div className="reviews__stars rating__stars">
-                              <span
-                                style={{
-                                  width: `${calculateRatingPercentage(
-                                    offer.rating
-                                  )}%`,
-                                }}
-                              >
-                              </span>
-                              <span className="visually-hidden">Rating</span>
-                            </div>
-                          </div>
-                          <p className="reviews__text">{review.text}</p>
-                          <time className="reviews__time" dateTime="2019-04-24">
-                            {review.date}
-                          </time>
-                        </div>
-                      </li>
+                      <ReviewItem
+                        key={review.id}
+                        review={review}
+                        offer={offer}
+                      />
                     ))}
                   </ul>
-                  <CommentForm />
-                </section>
-              )}
+                )}
+                <CommentForm />
+              </section>
             </div>
           </div>
           <section className="property__map map"></section>
