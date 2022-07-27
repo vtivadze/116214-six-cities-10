0e560b610b07850746c2d35c05563195375c91c9
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-27 16:22:03 +0400

    Create constant object cityCoordinates

--- project/src/constants.ts
+++ project/src/constants.ts
@@ -11,6 +11,33 @@ export enum AppRoute {
   Offer = '/offer/:id',
 }
 
+export const cityCoordinates = {
+  'Paris': {
+    lat: 48.91283939320031,
+    lng: 2.350601814812922,
+  },
+  'Cologne': {
+    lat: 50.998104496128335,
+    lng: 6.963300378398547,
+  },
+  'Brussels': {
+    lat: 50.95432774923024,
+    lng: 4.326333436354579,
+  },
+  'Amsterdam': {
+    lat: 52.36961548135118,
+    lng: 4.904384736819395,
+  },
+  'Hamburg': {
+    lat: 53.598960404627,
+    lng: 9.983960189970864,
+  },
+  'Dusseldorf': {
+    lat: 51.5225460047204,
+    lng: 6.813198467783883,
+  },
+};
+
 export const initialCoordinates = {
   lat: 52.36961548135118,
   lng: 4.904384736819395,
