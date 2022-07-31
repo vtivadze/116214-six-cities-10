3898b0f9be3915db3596799a269624b55e46a622
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-31 18:53:58 +0400

    Create SortingVariants component

--- project/src/pages/main-screen/main-screen.tsx
+++ project/src/pages/main-screen/main-screen.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import Header from '../../components/header/header';
 import OfferItem from '../../components/offer-item/offer-item';
+import SortingVariants from '../../components/sorting-variants/sorting-variants';
 import CityList from '../../components/city-list/city-list';
 import Map from '../../components/map/map';
 import { useAppSelector } from '../../hooks';
@@ -43,32 +44,7 @@ function MainScreen(): JSX.Element {
               <b className="places__found">
                 {cityOffers.length} places to stay in Amsterdam
               </b>
-              <form className="places__sorting" action="/" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li
-                    className="places__option places__option--active"
-                    tabIndex={0}
-                  >
-                    Popular
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: low to high
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: high to low
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Top rated first
-                  </li>
-                </ul>
-              </form>
+              <SortingVariants />
               <div className="cities__places-list places__list tabs__content">
                 {cityOffers.map((offer) => (
                   <OfferItem
