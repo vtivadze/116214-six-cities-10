df13d30204ffbfe162c0b70369feb7af114d1b2c
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-08-04 13:36:33 +0400

    Add server error handling

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -4,6 +4,9 @@ import { Provider } from 'react-redux';
 import App from './components/app/app';
 import { store } from './store';
 import ErrorMessage from './components/error-message/error-message';
+import { checkAuthAction } from './store/api-actions';
+
+store.dispatch(checkAuthAction());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
